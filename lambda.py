54a4b52565b37943ed194d8f9858e6693ff86285
michalurbanski <michal.urbanski@thomsonreuters.com>
2022-12-29 10:33:36 +0100

    ADO_450287 - [VIC] - Woody - PoC - Parse Metadata - changed logic for communication with S3

--- lambda.py
+++ lambda.py
@@ -1,19 +1,38 @@
 import os
-
-from xml_handler import *
+import boto3
+from botocore.exceptions import ClientError
+from xml_handler import handler
 
 
 def lambda_handler(event, context):
+    data = {}
+    file_name_sns = event["body"]["metadata"]["name"]
+    hive_material_id = event["body"]["metadata"]["materialId"]
+    s3 = boto3.client("s3")
+    data["file_name"] = file_name_sns
+    data["materialId"] = hive_material_id
 
-    content = event["Records"][0]["Sns"]["RequestData"]
-    with open("/tmp/tmpFile.xml", "w") as file:
-        file.write(content)
-    with open("/tmp/tmpFile.xml", "r"):
-        workflows = handler("workflow", "/tmp/tmpFile.xml")
-        sources = handler("sources", "/tmp/tmpFile.xml")
-        result = {
-            "workflow": workflows,
-            "sources": sources
-        }
-    os.system("rm /tmp/tmpFile.xml")
-    return result
+    if not file_in_s3_bucket(file_name_sns, s3):
+        data["workflows"] = None
+        data["sources"] = None
+        return data
+
+    s3resource = boto3.resource("s3")
+    file_path = f"/tmp/{file_name_sns}"
+    s3resource.Bucket("sh-woody-poc-xml").download_file(file_name_sns, file_path)
+    workflows = handler("workflow", file_path)
+    sources = handler("sources", file_path)
+
+    data["workflow"] = workflows
+    data["sources"] = sources
+
+    os.system(f"rm /tmp/{file_name_sns}")
+    return data
+
+
+def file_in_s3_bucket(file_name_sns, s3):
+    try:
+        s3.head_object(Bucket="sh-woody-poc-xml", Key=file_name_sns)
+    except ClientError:
+        return False
+    return True
